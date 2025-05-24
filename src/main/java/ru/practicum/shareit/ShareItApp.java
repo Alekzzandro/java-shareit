d9ce406b190017331bf2f7c32b6df520c01bed8c
Torchinskij Alexandr <alekzz02@mail.ru>
2025-05-24 20:33:19 +0500

    First upset try2!

--- src/main/java/ru/practicum/shareit/ShareItApp.java
+++ src/main/java/ru/practicum/shareit/ShareItApp.java
@@ -2,12 +2,12 @@ package ru.practicum.shareit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//это ******
+
 @SpringBootApplication
 public class ShareItApp {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ShareItApp.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ShareItApp.class, args);
+    }
 
 }
